60bd0163c82f023ca76f5563427de342b3eaecf6
Lucas Bordignon <bordi.lucas@gmail.com>
2019-09-08 23:14:53 -0300

    Add other models to paths

--- src/models.py
+++ src/models.py
@@ -1,4 +1,16 @@
 paths = {
+    'ssd_mobilenet_v1_coco': 'http://download.tensorflow.org/models' +
+                             '/object_detection/ssd_mobilenet_v1_coco' +
+                             '_2018_01_28.tar.gz',
+    'ssd_mobilenet_v2_coco': 'http://download.tensorflow.org/models'
+                             '/object_detection/ssd_mobilenet_v2_coco' +
+                             '_2018_03_29.tar.gz',
+    'ssd_inception_v2_coco': 'http://download.tensorflow.org/models' +
+                             '/object_detection/ssd_inception_v2_coco'
+                             '_2018_01_28.tar.gz',
+    'faster_rcnn_inception_v2_coco': 'http://download.tensorflow.org/models' +
+                                     '/object_detection/faster_rcnn ' +
+                                     '_inception_v2_coco_2018_01_28.tar.gz',
     'mask_rcnn_inception_v2_coco': 'http://download.tensorflow.org/models' +
                                    '/object_detection/mask_rcnn_inception' +
                                    '_v2_coco_2018_01_28.tar.gz'
